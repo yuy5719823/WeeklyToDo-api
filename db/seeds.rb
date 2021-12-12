23c1ebeadb315bc84e3f5a8755240501707c5d39
yuy5719823 <youya_827@yahoo.co.jp>
2021-12-12 22:00:46 +0900

    [Add]テストデータ

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#テストユーザーの作成
+User.create(
+  name: "testUser",
+  email: "test@example.com",
+  password: "password",
+  password_confirmation: "password",
+)
+
+#テストデータの作成
+user = User.find_by(email: "test@example.com")
+3.times do | number |
+  monthlyGoal = user.monthly_goals.create( goal: "testGoal_#{number}" )
+  2.times do | number |
+    monthlyGoal.monthly_memos.create( memo: "testMemo_#{number}" )
+  end
+end
+
+puts "テストデータの作成完了"
