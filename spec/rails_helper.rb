9cf3c2d387d3062441d72e57c228b71c40bdf79c
yuy5719823 <youya_827@yahoo.co.jp>
2021-12-14 21:30:18 +0900

    [Edit]loginヘルパーを利用できるように

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,8 +7,6 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 require 'devise'
 # Add additional requires below this line. Rails is not loaded until this point!
-require File.expand_path("spec/support/controller_macros.rb")
-require_relative 'support/controller_macros'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -40,6 +38,7 @@ RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::IntegrationHelpers, type: :request
   config.extend ControllerMacros, :type => :controller
+  config.include LoginMacros
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
