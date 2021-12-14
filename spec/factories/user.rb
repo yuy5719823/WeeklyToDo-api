17e2197d359878ad012b022b7450dfbb809ac15f
yuy5719823 <youya_827@yahoo.co.jp>
2021-12-14 21:32:15 +0900

    [Edit]ユーザー情報の追加

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -5,6 +5,13 @@ FactoryBot.define do
     password              { "password" }
     password_confirmation { "password" }
 
+    factory :client, class: 'user' do
+      name                  { "client" }
+      email                 { "client@example.com" }
+      password              { "password" }
+      password_confirmation { "password" }
+    end
+
     # trait :user_with_monthly_goals do
     #   after(:build) do | user |
     #     user.groups 
