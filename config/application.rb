b18409f8ae2086b046b41573a0965e79c7ee6740
yuy5719823 <youya_827@yahoo.co.jp>
2021-12-16 21:53:42 +0900

    [Edit]タイムゾーンを指定

--- config/application.rb
+++ config/application.rb
@@ -28,8 +28,8 @@ module Api
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
+    #タイムゾーンを指定
+    config.time_zone = "Tokyo"
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Only loads a smaller set of middleware suitable for API only apps.
