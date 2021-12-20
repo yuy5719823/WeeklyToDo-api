38c49922d7fea710f7cd56d9bb70d24dbd479f64
yuy5719823 <youya_827@yahoo.co.jp>
2021-12-20 22:17:28 +0900

    [Edit]週の目標のテストデータを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,20 +7,28 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 #テストユーザーの作成
-User.create(
+user = User.new(
   name: "testUser",
   email: "test@example.com",
   password: "password",
   password_confirmation: "password",
 )
+#メール認証をスキップ
+user.skip_confirmation!
+user.save
 
 #テストデータの作成
-user = User.find_by(email: "test@example.com")
+#月の目標、メモを作成
 3.times do | number |
-  monthlyGoal = user.monthly_goals.create( goal: "testGoal_#{number}" )
+  monthlyGoal = user.monthly_goals.create( goal: "testMonthlyGoal_#{number}" )
   2.times do | number |
-    monthlyGoal.monthly_memos.create( memo: "testMemo_#{number}" )
+    monthlyGoal.monthly_memos.create( memo: "testMonthlyMemo_#{number}" )
   end
 end
+#週の目標を作成
+3.times do | number |
+  weekly_goal = user.weekly_goals.create( goal: "testWeeklyGoal_#{number}")
+end
+
 
 puts "テストデータの作成完了"
